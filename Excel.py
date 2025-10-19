ab99e881c73876923c54a6c5d75276e83b764acb
Almas <almasputri77@gmail.com>
2025-10-19 13:39:39 +0800

    update

--- Excel.py
+++ Excel.py
@@ -4,15 +4,22 @@
 # Short description of the task
 #
 
+import pandas as pd
 
-
-#Input
-import pandas as pd 
-
-df = pd.read_excel ('Finansial_Sample.xlsx') 
+# 1. Input
+df = pd.read_excel ('Financial_Sample1.xlsx') 
 
 # 2. Process
+sum = df.select_dtypes(include='number').sum()
+sums = df.select_dtypes(include="number").sum()
+
+# Optionally give a label for the row (e.g.'Total')
+sums['Name'] = "Total" #Add value for the non- numeric.colomn
+
+# Append the total of row to DataFrame
+df_with_total = pd.concat ([df,pd.DataFrame([sums])], ignore_index=True)
 
 # 3. Output
-print(df)
+print(df_with_total)
+df_with_total.to_excel('output.xlsx', Index=False)
 
---
+++ Excel2.py
@@ -0,0 +1,22 @@
+#
+# Almas, 2025/10/08
+# File: Excel.py
+# Short description of the task
+#
+
+import pandas as pd
+
+#Input
+files = ["Financial_Sample1" , "Financial_Sample2"]
+
+# 2. Process
+
+
+# 3. Output
+print (f"Processing {file}")
+
+if i len(files) - 1:
+print("This is the last file!")
+
+
+
