f667d536064cff877bda08276c0786a86becb904
Almas <almasputri77@gmail.com>
2025-10-19 12:40:46 +0800

    update

---
+++ Excel.py
@@ -0,0 +1,18 @@
+#
+# Almas, 2025/10/08
+# File: Excel.py
+# Short description of the task
+#
+
+
+
+#Input
+import pandas as pd 
+
+df = pd.read_excel ('Finansial_Sample.xlsx') 
+
+# 2. Process
+
+# 3. Output
+print(df)
+
