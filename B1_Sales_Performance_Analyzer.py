7daaae772cdda00dfb97fc93e6dc713f2421b533
Almas <almasputri77@gmail.com>
2025-10-20 16:55:14 +0800

    update

--- B1_Sales_Performance_Analyzer.py
+++ B1_Sales_Performance_Analyzer.py
@@ -41,9 +41,7 @@ for i in range(len(df)):
 
     total_bonus += bonus
 
-    print(name,": Target", status, "| Sales: $", f"{sales:,.0f}", "| Bonus: $", f"{bonus:,.0f}")
+    print(f"{name}: Target {status} | Sales: ${sales:,.0f} | Bonus: ${bonus:,.0f}")
 
-print()
-print("Total Bonuses to Pay: $", f"{total_bonus:,.0f}")
-
-df.to_excel('sales_data_with_bonus.xlsx', index=False)
+print()                                                                              #enter       
+print(f"Total Bonuses to Pay: ${total_bonus:,.0f}")
---
+++ B2_Inventory_Reorder_System.py
@@ -0,0 +1,26 @@
+#
+# Almas, 2025/10/20
+# File: Inventory_Reorder_System.py
+# Short description of the task
+#
+
+import pandas as pd
+
+# 1. Input
+df = pd.read_excel ('inventory.xlsx') 
+
+# 2. Process
+#Check if the current stock is below the minimum stock
+for i in range(len(df)):
+
+product_name = df.loc[i, 'Product_Name']
+current_stock = df.loc[i, 'Current_Stock']
+minimum_stock = df.loc[i, 'Minimum_Stock']   
+
+if current_stock < minimum_stock:
+    print(product_name, "needs reorder. Current stock:", current_stock, "| Minimum stock:", minimum_stock)
+else:
+    print(product_name, "is in good stock. Current stock:", current_stock, "| Minimum stock:", minimum_stock)
+
+# 3. Output
+
