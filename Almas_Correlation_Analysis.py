f0e9cc0ce1e24aa7966665fb08747a60b4c40137
Almas <almasputri77@gmail.com>
2025-10-22 17:05:50 +0800

    update

--- Almas_Correlation_Analysis.py
+++ Almas_Correlation_Analysis.py
@@ -6,16 +6,38 @@
 
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from scipy import stats
 
 # 1. Input
-df = pd.read_csv('Simple_Data.csv')
+df = pd.read_csv('Correlation_Analysis_Data.csv')
+
+df.info()
+
+# correlation, pvalue = stats.pearsonr(df['Marketing_Spend'], df['Sales_Revenue'])
+
+correlation_matrix = df.iloc[:,1:6].corr()
+print(correlation_matrix.round(3))
+
 
 # 2. Process
-print(df.isnull().sum())
-print(df.isnull().sum().sum())
+# df[].corr()
+
+# print(df.isnull().sum())
+# print(df.isnull().sum().sum())
 
 
 
 # 3. Output
-print ('Data loaded successfully!')
-print (f'Dataset shape:{df.shape}')
+# print ('Data loaded successfully!')
+# print (f'Dataset shape:{df.shape}')
+# print(f'Correlation: {correlation:.2f}')
+# print(f'P value: {pvalue:.4e}') 
+
+sns.heatmap(correlation_matrix)
+plt.title('Almas is the most intelligent person in the wolrd')
+plt.tight_layout()
+plt.show()
