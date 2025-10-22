38acfbbd5d48e7f3eef82563f26376e65aab2b50
Almas <almasputri77@gmail.com>
2025-10-22 16:03:53 +0800

    update

--- Almas_Correlation_Analysis.py
+++ Almas_Correlation_Analysis.py
@@ -5,9 +5,17 @@
 # Apply correlation analysis to business problem
 
 
-# 1. Input
+import pandas as pd
 
+# 1. Input
+df = pd.read_csv('Simple_Data.csv')
 
 # 2. Process
+print(df.isnull().sum())
+print(df.isnull().sum().sum())
+
+
 
 # 3. Output
+print ('Data loaded successfully!')
+print (f'Dataset shape:{df.shape}')
